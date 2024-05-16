9de2b2c14193c6c1bd4165d3c50e618b52c59aa9
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-05-16 16:51:57 +0200

    refactor: update Product component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,24 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ data, ...props }) => {
-  const { name, _id, image, price } = data;
+  const { name, _id, image, price, rating, numReviews } = data;
 
   return (
     <Card className={'my-3 p-3 rounded'} {...props}>
-      <Link to={`/products/${_id}`}>
+      <Link to={`/product/${_id}`}>
         <Card.Img src={image} variant={'top'} />
       </Link>
       <Card.Body>
-        <Link to={`/products/${_id}`}>
-          <Card.Title as={'div'}>
+        <Link to={`/products/${_id}`} title={name}>
+          <Card.Title as={'div'} className={'product-title'}>
             <strong>{name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as={'div'}>
+          <Rating value={rating} text={`${numReviews} review${numReviews > 1 ? 's' : ''}`} />
+        </Card.Text>
         <Card.Text as={'h3'}>${price}</Card.Text>
       </Card.Body>
     </Card>
