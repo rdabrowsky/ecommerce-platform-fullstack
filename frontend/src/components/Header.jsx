44a2b2e81655c0707c72fc52abd56e131634a21f
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-05-16 13:32:08 +0200

    refactor: refactor links elements

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 
@@ -7,21 +8,27 @@ const Header = () => {
     <header>
       <Navbar bg={'dark'} variant={'dark'} expand={'md'} collapseOnSelect>
         <Container>
-          <Navbar.Brand href={'/'}>
-            <img src={logo} alt={'ProShop logo'} />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to={'/'}>
+            <Navbar.Brand href={'/'}>
+              <img src={logo} alt={'ProShop logo'} />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           <NavbarToggle aria-controls={'basic-navbar-nav'} />
           <Navbar.Collapse id={'basic-navbar-nav'}>
             <Nav className={'ms-auto'}>
-              <Nav.Link href={'/cart'}>
-                <FaShoppingCart />
-                Cart
-              </Nav.Link>
-              <Nav.Link href={'/login'}>
-                <FaUser />
-                Sign in
-              </Nav.Link>
+              <LinkContainer to={'/cart'}>
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to={'/login'}>
+                <Nav.Link>
+                  <FaUser />
+                  Sign in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
