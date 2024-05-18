1a76405738c4af7d6620b0591f9fc4556ef6681a
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-05-18 18:29:03 +0200

    feat: Install connect app with db

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import connectDB from './config/database.js';
 import products from './data/products.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
