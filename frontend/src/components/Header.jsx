2a6dce708abf1bcf456b0097dd04ce038cea9805
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-05-23 22:01:01 +0200

    feat: Add badge with counted cart items

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavbarToggle, Badge } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg={'dark'} variant={'dark'} expand={'md'} collapseOnSelect>
@@ -21,6 +24,11 @@ const Header = () => {
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg={'success'} style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to={'/login'}>
