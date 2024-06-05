87b2c838d1d28a534c51337a549d494eac51471d
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-06-05 12:57:10 +0200

    feat: create getUserProfile and updateUserprofile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,18 +77,48 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  // const users = await User.find({});
+  const user = await User.findById(req.user._id);
 
-  res.send('get user profile  user');
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  // const users = await User.find({});
+  const user = await User.findById(req.user._id);
 
-  res.send('update user profile  user');
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Get users
