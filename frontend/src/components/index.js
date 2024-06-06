908f41869e5e56c45ef131808aaaddc3a46ab3a3
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-06-06 15:10:48 +0200

    feat: Create checkout steps

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -7,3 +7,4 @@ export { default as Message } from './Message';
 export { default as Product } from './Product';
 export { default as Rating } from './Rating';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as CheckoutsSteps } from './CheckoutsSteps';
--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { saveShippingAddress } from '../slices/cartSlice';
-import { FormContainer } from '../components';
+import { CheckoutsSteps, FormContainer } from '../components';
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -26,6 +26,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutsSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId={'address'} className={'my-2'}>
