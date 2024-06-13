ec20c058fc469c5532b57b7c1a38dc86120d5fd9
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-06-13 18:02:49 +0200

    feat: Add controller for createing new product (wip)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getProductById, getProducts } from '../controllers/productController.js';
+import { createProduct, getProductById, getProducts } from '../controllers/productController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router;
